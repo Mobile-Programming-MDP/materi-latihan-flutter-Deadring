9dfa0273c82d36c2fed28d5bac252f3cee25ad8e
Deadring <deadring541@gmail.com>
2023-10-16 18:20:32 +0700

    Update main.dart

--- halo_widget/lib/main.dart
+++ halo_widget/lib/main.dart
@@ -10,49 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Klee",
-      theme: ThemeData(primarySwatch: Colors.lime),
-      home: FirstScreen
-    );
+        title: "Hallo Widget",
+        theme: ThemeData(primarySwatch: Colors.red),
+        home: const FirstScreen()
+        );
   }
 }
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen
-  ({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Klee"),
-          actions: <Widget>[IconButton(
-            onPressed: () {},
-            icon:  const Icon(
-              Icons.search,
-              color: Colors.blue,
-            ),
-          )],
-          leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.home,
-              color: Colors.deepOrange,
-            ),
-          ),
-        ),
-        body: const Center(
-          child: Text("Halo Klee"),
-        ),
-      ),
-    ),
-  };
-}
-
-class WidgetDemo extends StatelessWidget{
+class WidgetDemo extends StatelessWidget {
   const WidgetDemo({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Widget Demo'),
@@ -60,47 +28,85 @@ class WidgetDemo extends StatelessWidget{
       body: Column(
         children: [
           Container(
-            height:  200,
+            height: 200,
             margin: const EdgeInsets.all(16),
             padding: const EdgeInsets.all(16),
             color: Colors.blue,
             child: const Center(
               child: Text(
-                'ini adalah contoh penggunaan container',
+                'Ini adalah contoh penggunaan Container',
                 style: TextStyle(
                   fontSize: 20.0,
-                  color: Colors.amber,
+                  color: Colors.white,
                 ),
                 textAlign: TextAlign.center,
               ),
             ),
           ),
           ElevatedButton(
-            onPressed: (){},
+            onPressed: () {},
             child: const Padding(
               padding: EdgeInsets.all(16.0),
               child: Text('Tombol Elevated'),
             ),
-          ), 
+          ),
           const SizedBox(
             height: 10,
           ),
           const Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(Icons.star, color: Colors.amber),
-              Text('Rating : 4.5'),
+              Icon(
+                Icons.star,
+                color: Colors.amber,
+              ),
+              Text('Rating: 4.5'),
             ],
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Image.network('https://picsum.photos/id/7/300/200',
-            width: double.infinity,
-            height: 200,
-            fit: BoxFit.cover,),
+            child: Image.network(
+              'https://picsum.photos/id/7/300/200',
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
           )
         ],
       ),
     );
   }
+}
+
+class FirstScreen extends StatelessWidget{
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+        title: const Text("Klee"),
+        actions: <Widget>[
+          IconButton(
+            onPressed:(){}, 
+            icon: const Icon(
+              Icons.search, 
+              color: Colors.white,
+          ))
+        ],
+        leading: IconButton(
+          //icon di kiri appbar
+          onPressed: (){},
+          icon: const Icon(
+            Icons.home,
+            color: Colors.white,
+          ),
+        ),
+        ),
+          body: const Center(
+            child: Text("Hallo Klee"),
+          ),
+        );
+  }
+
 }
